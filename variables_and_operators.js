442756105709353fdac6dd525d69a5a89ac7f396
PortaroMarco <marco_portaro@hotmail.es>
2025-07-10 13:30:08 -0300

    primer commit

---
+++ variables_and_operators.js
@@ -0,0 +1,17 @@
+// 1.a Crear dos variables numéricas y sumarlas
+var numero1 = 10;
+var numero2 = 20;
+var resultadoSuma = numero1 + numero2;
+console.log("1.a) Resultado de la suma:", resultadoSuma);
+
+// 1.b Crear dos strings y concatenarlos
+var palabra1 = "Hola";
+var palabra2 = "Mundo";
+var frase = palabra1 + " " + palabra2;
+console.log("1.b) Frase:", frase);
+
+// 1.c Sumar el largo de dos strings
+var texto1 = "JavaScript";
+var texto2 = "Ejercicios";
+var sumaDeLargos = texto1.length + texto2.length;
+console.log("1.c) Total de letras:", sumaDeLargos);
